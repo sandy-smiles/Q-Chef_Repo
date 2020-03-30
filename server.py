599a9250154d4c744d047bcf7106a9925cdf8f65
Sandy <sandy.dibona101010@gmail.com>
2020-03-30 11:32:40 +1100

    heroku-server: Change wrapper to allow POST requests? Change wrapper to allow POST and also added the data within a folder.
    On branch develop_heroku
    created: data/*
    modified: server.py

--- server.py
+++ server.py
@@ -15,54 +15,57 @@ app = Flask(__name__)
 # API URLs
 ##########################################
 # API index - shows when people visit the home page
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return 'Hello!\nYou have reached the backend API for Q-Chef.'
+  return 'Hello!\nYou have reached the backend API for Q-Chef.'
 
 # API about - page about contacts that might be needed?
-@app.route('/about')
+@app.route('/about', methods=['GET'])
 def about():
-    return 'Q-Chef is ...'
+  return 'Q-Chef is ...'
 
 # API save diet req - saving the user's dietary requirements
-@app.route('/diet_req')
+@app.route('/diet_req', methods=['GET', 'POST'])
 def about():
+  try:
     requ = request.json
     resp = {}
     resp[user_id] = requ["user_id"]
 
-    return jsonify(info)
+    return jsonify(resp)
+  except:
+    return f'Unable to grab the request'
 
 ##########################################
 # Testing URLs
 ##########################################
 # API pref - returns json list of recipes
 # TODO(kbona@): Return a list of 10 pref recipes.
-@app.route('/pref/<id>')
+@app.route('/pref/<id>', methods=['GET', 'POST'])
 def taste_preference(id):
-    err = checkID(id)
-    if err:
-        return f'Unable to find user {id}. Unable to return a list of preferenced recipes. err = {err}'
+  err = checkID(id)
+  if err:
+    return f'Unable to find user {id}. Unable to return a list of preferenced recipes. err = {err}'
 
-    userPreferenced = {10: "Chicken Wrap", 8: "Gnocchi", 4: "Salmon Steak", 42: "Roti Canai"}
-    userRecipes = {} # grab dict of user's recipes not rated from db
-    allowedRecipes = userRecipes.keys()
-    picked = -1 # index not allowed.
+  userPreferenced = {10: "Chicken Wrap", 8: "Gnocchi", 4: "Salmon Steak", 42: "Roti Canai"}
+  userRecipes = {} # grab dict of user's recipes not rated from db
+  allowedRecipes = userRecipes.keys()
+  picked = -1 # index not allowed.
 
-    """
-        for i in range(10):
-            while picked != -1: # tolerance > choice:
-                picked = rand.Random(allowedRecipes)
-                allowedRecipes.remove(picked)
-            userPreferenced[picked] = userRecipes[picked]
-            picked = -1 # TODO(kbona@): Remove once finalised while loop logic.
-    """
+  """
+      for i in range(10):
+          while picked != -1: # tolerance > choice:
+              picked = rand.Random(allowedRecipes)
+              allowedRecipes.remove(picked)
+          userPreferenced[picked] = userRecipes[picked]
+          picked = -1 # TODO(kbona@): Remove once finalised while loop logic.
+  """
 
-    return jsonify(userPreferenced)
+  return jsonify(userPreferenced)
 
 ##########################################
 # Server Activation
 ##########################################
 # Start the web server!
 if __name__ == "__main__":
-    app.run()
+  app.run()
