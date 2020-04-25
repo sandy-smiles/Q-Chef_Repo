5a5164d445c740087f637c9170b204134e0419ac
Sandy <sandy.dibona101010@gmail.com>
2020-04-25 17:57:10 +1000

    heroku-images-server: Small server to help server the images needed for the application.
    On branch heroku_images
    added: images/*+.jpg
    modified: server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 
 # Create a web server
 app = Flask(__name__)
+imgStrLen = 5
 
 ######## API URLs ########
 # API index - shows when people visit the home page
@@ -10,9 +11,13 @@ def home():
     return 'Hello!\nYou have reached the backend API for Q-Chef.'
 
 # API about - page about contacts that might be needed?
-@app.route('/about')
-def about():
-    return 'Q-Chef is ...'
+@app.route('/image/<imgId>')
+def images(imgId):
+  try:
+    filename = f'{"./images/"+"0"*(imgStrLen-len(imgId))+imgId}.jpg'
+    return send_file(filename, mimetype='image/jpg')
+  except:
+    return f'Unable to find an image for {imgId}.'
 
 ######## Server Activation ########
 # Start the web server!
