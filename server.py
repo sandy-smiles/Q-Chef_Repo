264ac841ee23ff5e6c52cae717c3375e8791ea51
Sandy <sandy.dibona101010@gmail.com>
2020-06-01 13:37:33 +1000

    AllPancakes: Updating server to be able to return a differing number of recipes/ingredient data for the pancakes version.

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 ################################################################################
 # Imports and application creation
 ################################################################################
+import os
 import json
 import requests
 from flask import Flask, request, jsonify, render_template, redirect, url_for
@@ -41,85 +42,8 @@ userStartingDoc = {
   'pickedRecipes': []
 }
 
-exampleIngredientDict = """
-{"338": "tortillas/pita/flat breads", 
- "160": "horseradish", 
- "93": "coconut"}
-"""
-
-exampleRecipeDict = """
-{"60372": {
-  "cookTime": 30,
-  "ingredient_phrases": [
-    "450g/1lb boneless chicken breasts, skinned, cut into 2.5cm/1in cubes",
-    "2 tsp light soy sauce",
-    "1 tsp Shaoxing rice wine (or dry sherry)",
-    "1 tsp cornflour",
-    "1 tbsp groundnut (peanut) oil",
-    "1 tsp salt",
-    "freshly ground white pepper",
-    "2 tbsp finely chopped orange zest",
-    "1 tbsp finely chopped lemon zest",
-    "2 tsp sesame oil",
-    "3 tbsp finely chopped fresh coriander",
-    "enough long-grain rice to fill a measuring jug to 400ml/14fl oz level",
-    "1 tbsp groundnut or peanut oil",
-    "3 garlic cloves, finely sliced",
-    "2 tsp salt",
-    "750g/1.5lb Chinese greens, such as choi sum or bok choi"
-  ],
-  "prepTime": 30,
-  "servings": "Serves 4",
-  "steps": [
-    "Combine the cubed chicken with the soy sauce, rice wine (or dry sherry) and cornflour in a small bowl. Put the mixture in the fridge for about 15 minutes.",
-    "For the rice, put the rice into a heavy pan with 600ml/21 fl oz water. The general rule of thumb is that the water should come up above the level of the rice by about 2.5cm/1in, or the top part of the thumb!",
-    "Bring the water to the boil and cook until most of the surface liquid has evaporated - this should take about 15 minutes. The surface of the rice should have small indentations like a pitted crater.",
-    "At this point, cover the pan with a very tight-fitting lid, turn the heat as low as possible and let the rice cook undisturbed for 15 minutes.  There is no need to 'fluff' the rice, let it rest for five minutes before serving it.",
-    "To finish the chicken, heat a wok until it is very hot and then add the oil. When the oil is very hot and slightly smoking, add the chicken to the wok, together with the salt, pepper and orange and lemon zest.",
-    "Stir-fry the mixture for four minutes, or until the chicken is cooked.  Stir in the sesame oil and give the mixture two turns and cook for another three minutes. Finally add the coriander and continue to stir-fry for another minute. Turn onto a platter and serve at once.",
-    "For the greens, heat a wok or large frying-pan over high heat until it is hot. Add the oil, and, when it is very hot and slightly smoking, add the garlic and salt. Stir-fry the mixture for 15 seconds. Quickly add the Chinese greens.  Stir-fry for 3-4 minutes, or until the greens have wilted, but are still slightly crisp.",
-    "Serve the chicken with the Chinese greens and rice."
-  ],
-  "title": "Quick orange and lemon chicken"},
-"27455": {
-  "cookTime": 120,
-  "ingredient_phrases": [
-    "4 tbsp vegetable oil",
-    "500g/1lb 2oz good-quality beef steak, finely diced",
-    "500g/1lb 2oz good-quality beef mince",
-    "1 white onion, peeled, finely chopped",
-    "1 red onion, peeled, finely chopped",
-    "2 sticks celery, trimmed, chopped",
-    "1 dried chipotle chilli",
-    "1 tbsp dried chilli flakes",
-    ".5 tbsp chilli powder",
-    "2 tsp dried oregano",
-    "3 tbsp light brown sugar",
-    "2 x 400g/14oz cans chopped tomatoes",
-    "500ml/17fl oz beef stock",
-    "1 x 400g/14oz can kidney beans, drained and rinsed",
-    "1 x 400g/14oz can black-eyed beans, drained and rinsed",
-    "sea salt flakes and freshly ground black pepper",
-    "75g/3oz plain chocolate, minimum 70% cocoa solids, roughly chopped",
-    "bunch fresh coriander",
-    "steamed rice",
-    "8 tbsp soured cream",
-    "4 spring onions, trimmed, finely sliced"
-  ],
-  "prepTime": 30,
-  "servings": "Serves 6-8",
-  "steps": [
-    "Heat two tablespoons of the oil over a medium to high heat in a large, heavy-based pan with a tight fitting lid.",
-    "Fry the diced beef steak in batches until browned all over, setting each batch aside on a plate using a slotted spoon.",
-    "Add another tablespoon of oil to the pan and add the beef mince, frying until browned all over. Remove the mince from the pan using a slotted spoon and set aside with the beef steak.",
-    "Add the remaining tablespoon of oil to the pan and fry the white onion, red onion and celery for 3-4 minutes, or until the onions have softened but not browned.",
-    "Stir in the chipotle chilli, chilli flakes, chilli powder and oregano until well combined. Cook for a further two minutes.",
-    "Return the diced and minced beef to the pan, then stir in the sugar, chopped tomatoes, beef stock, kidney beans and black-eyed peas. Bring the mixture to the boil, then reduce the heat until the mixture is simmering. Cover and continue to simmer over a low heat for 2-3 hours. (Alternatively, preheat the oven to 140C/280F/Gas 1 to cook in an ovenproof dish for the same amount of time.)",
-    "Just before serving, season the chilli with crushed sea salt flakes and freshly ground pepper. Stir in the chocolate until melted, then stir in the chopped coriander.",
-    "Serve the chilli with a bowl of steamed rice and top each serving with a dollop of soured cream. Garnish with the sliced spring onions."
-  ],
-  "title": "Beef chilli with bitter chocolate"}}
-"""
+relativeIngredientsFilePath = "./tmp_ingredients"
+relativeRecipesFolderPath = "./tmp_recipes"
 
 ################################################################################
 # Debugging
@@ -150,6 +74,58 @@ def debug(fString):
   if DEBUG and HELP and 'HELP' in fString:
     print(fString)
     return
+
+################################################################################
+# Helper Functions
+################################################################################
+# getExampleIngredients - Input: Number of ingredients, Output: Dictionary
+def getExampleIngredients(n):
+  ingredients = {}
+  with open(relativeIngredientsFilePath, 'r') as f:
+    for line in f:
+      ingredient_id, ingredient_name = line.strip().split(listDelimiter)
+      ingredients[ingredient_id] = ingredient_name
+
+  if n == -1:
+    n = len(ingredients.keys())
+
+  r = {}
+  for i, ingredient_id in enumerate(ingredients.keys()):
+    if i > n:
+      break
+    r[ingredient_id] = ingredients[ingredient_id]
+
+  return r
+
+################################################################################
+# getExampleRecipes - Input: Number of recipes, Output: Dictionary
+def getExampleRecipes(n):
+  recipe_ids = []
+  for rootFolder, directories, fileNames in os.walk(relativeRecipesFolderPath):
+    fileNames.sort()
+    recipe_ids = fileNames
+    break
+  print(recipe_ids)
+
+  if n == -1:
+    n = len(recipe_ids)
+    print(f"n = {n}")
+
+  r = {}
+  for i, recipe_id in enumerate(recipe_ids):
+    print(f"i = {i}, recipe_id = {recipe_id}")
+    if i > n:
+      print("Breaking... i = {i}, n = {n}")
+      break
+    fileName = f"{relativeRecipesFolderPath}/{recipe_id}"
+    print(f"Attempting to open file {fileName}")
+    with open(fileName, 'r') as f:
+      print(f"recipe_id = {recipe_id}")
+      recipe_dic = json.load(f)
+      print(f"recipe_dic = {recipe_dic}")
+      r[recipe_id] = recipe_dic
+
+  return r
 
 ################################################################################
 # API URLs
@@ -193,7 +169,7 @@ def onboarding_ingredient_rating():
       return ''
 
     debug('[onboarding_ingredient_rating - INFO]: GET request')
-    return jsonify(json.loads(exampleIngredientDict))
+    return jsonify(getExampleIngredients(-1))
   except:
     return f"[onboarding_recipe_rating - ERROR]: Something went wrong..."
   return ""
@@ -222,10 +198,10 @@ def onboarding_recipe_rating():
       debug('[onboarding_recipe_rating - INFO]: POST request')
       request_data = json.loads(request.data)
       debug(f'[onboarding_recipe_rating - DATA]: request_data: {request_data}')
-      return jsonify(json.loads(exampleRecipeDict))
+      return jsonify(getExampleRecipes(-1))
 
     debug('[onboarding_recipe_rating - INFO]: GET request')
-    return jsonify(json.loads(exampleRecipeDict))
+    return jsonify(getExampleRecipes(-1))
   except:
     return f"[onboarding_recipe_rating - ERROR]: Something went wrong..."
   return ""
@@ -271,7 +247,7 @@ def get_meal_plan_selection():
       request_data = json.loads(request.data)
       debug(f'[get_meal_plan_selection - DATA]: request_data: {request_data}')
 
-      return jsonify(json.loads(exampleRecipeDict))
+      return jsonify(getExampleRecipes(10))
   except:
     return f"[get_meal_plan_selection - ERROR]: Something went wrong..."
   return ""
@@ -314,8 +290,7 @@ def retrieve_meal_plan():
       debug('[retrieve_meal_plan - INFO]: POST request')
       request_data = json.loads(request.data)
       debug(f'[retrieve_meal_plan - DATA]: request_data: {request_data}')
-
-      return jsonify(json.loads(exampleRecipeDict))
+      return jsonify(getExampleRecipes(3))
   except:
     return f"[retrieve_meal_plan - ERROR]: Something went wrong..."
   return ""
